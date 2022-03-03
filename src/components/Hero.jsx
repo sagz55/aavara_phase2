e5e68398b165be20be5e391cffd6b8a1e99e73cc
sagar padayattil <sagz@sagars-MacBook-Air.local>
2022-03-03 09:23:33 +0530

    2nd

---
+++ src/components/Hero.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import scrollreveal from "scrollreveal";
+import styled from "styled-components";
+import homeImage from "../assets/hero.png";
+export default function Hero() {
+  const scroll =(e)=>{
+    scrollreveal().reveal('#recommend')
+  }
+  return (
+    <Section id="hero">
+      <div className="background">
+        <img src={homeImage} alt="" />
+      </div>
+      <div className="content">
+        <div className="title">
+          <h1>AAVARA</h1>
+          <p>
+            "Not all those who wander are lost" <br />
+                        Atleast not while we are around :)
+          </p>
+        </div>
+        <div className="search">
+          <div className="container">
+            <label htmlFor="">Enter destinatio</label>
+            <input type="text" placeholder="Search..." />
+          </div>
+          <div className="container">
+            <label htmlFor="">Leaving On</label>
+            <input type="date" />
+          </div>
+          <div className="container">
+            <label htmlFor="">Returning</label>
+            <input type="date" />
+          </div>
+          <div className="container">
+            <label htmlFor="">Total Budget</label>
+            <input type="text" placeholder="Amound" />
+          </div>
+          <button ><a href="#recommend">Explore Now</a> </button>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  position: relative;
+  margin-top: 2rem;
+  width: 100%;
+  height: 100%;
+
+  .background {
+    height: 100%;
+    img {
+      width: 100%;
+      filter: brightness(60%);
+    }
+  }
+  .content {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 3;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    .title {
+      color: white;
+      
+      h1 {
+        font-size: 6rem;
+        letter-spacing: 0.2rem;
+        font-family:'Ananda Neptouch 2',san-serif;
+      }
+      p {
+        text-align: center;
+        padding: 0 30vw;
+        margin-top: 0.5rem;
+        font-size: 1.2rem;
+      }
+    }
+    .search {
+      display: flex;
+      background-color: #ffffffce;
+      padding: 0.5rem;
+      border-radius: 0.5rem;
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 0 1.5rem;
+        label {
+          font-size: 1.1rem;
+          color: #03045e;
+        }
+        input {
+          background-color: transparent;
+          border: none;
+          text-align: center;
+          color: black;
+          &[type="date"] {
+            padding-left: 3rem;
+          }
+
+          &::placeholder {
+            color: black;
+          }
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+      a{
+        text-decoration:none;
+        color:black;
+      }
+      button {
+        padding: 1rem;
+        cursor: pointer;
+        border-radius: 0.3rem;
+        border: none;
+        color: white;
+        background-color: #4361ee;
+        font-size: 1.1rem;
+        text-transform: uppercase;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #023e8a;
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 980px) {
+    height: 25rem;
+    .background {
+      background-color: palegreen;
+      img {
+        height: 100%;
+      }
+    }
+    .content {
+      .title {
+        h1 {
+          font-size: 1rem;
+        }
+        p {
+          font-size: 0.8rem;
+          padding: 1vw;
+        }
+      }
+      .search {
+        flex-direction: column;
+        padding: 0.8rem;
+        gap: 0.8rem;
+        /* padding: 0; */
+        .container {
+          padding: 0 0.8rem;
+          input[type="date"] {
+            padding-left: 1rem;
+          }
+        }
+        button {
+          padding: 1rem;
+          font-size: 1rem;
+        }
+        /* display: none; */
+      }
+    }
+  }
+`;
---
+++ src/components/Services.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { VscNoNewline } from "react-icons/vsc";
+import styled from "styled-components";
+import service1 from "../assets/service1.png";
+import service2 from "../assets/service2.png";
+import service3 from "../assets/service3.png";
+
+export default function Services() {
+  const data = [
+    {
+      icon: service1,
+      title: "Stay Budget",
+      subTitle:
+        "Let us know how much you are willing to spend on your stay.",
+    },
+    {
+      icon: service2,
+      title: "Food Budget",
+      subTitle:
+        "Let us know how much you are willing to spend on your meals.",
+    },
+    {
+      icon: service3,
+      title: "Event Budget",
+      subTitle:
+        "Let us know how much you want to spend on various events.",
+    },
+    
+  ];
+  
+function Services() {
+    const data = [
+        {
+          icon: VscNoNewline,
+          title: "Pie chart",
+        },
+    ];
+  }
+      
+  return (
+    <Section id="services">
+      {data.map((service, index) => {
+        return (
+          <div className="service">
+            <div className="icon">
+              <img src={service.icon} alt="" />
+            </div> 
+            <h3>{service.title}</h3>
+            <div>
+            <input type="text" placeholder=" " />
+            </div>
+            <p>{service.subTitle}</p>
+          </div>
+        );
+      })}
+      <div className="service">
+            
+          </div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  padding: 5rem 0;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+  .service {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem;
+    background-color: aliceblue;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    transition: 0.3s ease-in-out;
+    &:hover {
+      transform: translateX(0.4rem) translateY(-1rem);
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+    .icon {
+      img {
+        height: 2.4rem;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 720px) {
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  }
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
